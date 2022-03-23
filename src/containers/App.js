ac878da3fe3154a44198cba66ba160e6997ec7d2
Pravin Nimmagadda <pravin.nimmagadda@and.digital>
2022-03-23 17:20:05 +0000

    comment to check push permision

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import { Accordion } from "../components/Accordion";
 import { NavBar } from "../components/NavBar";
 import AdminPage from "../components/AdminPage";
 import "bootswatch/dist/flatly/bootstrap.min.css";
-
+// this is comment to check if pravin can push to bootstrap_feature branch.
 function App() {
   return (
     <div className='d-flex align-items-start'>
